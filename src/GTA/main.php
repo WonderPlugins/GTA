9822700a7f47683890c005152dc8daf8240dfc9d
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2016-02-20 11:53:18 +0400

    Create main.php

---
+++ src/GTA/main.php
@@ -0,0 +1,12 @@
+<?php
+
+use pocketmine\plugin\pluginbase;
+use pocketmine\command\enchant;
+use pocketmine\item;
+use pocketmine\tile\sign;
+
+
+public function GameStart();
+$player->getPlayer();
+$player->getName();
+$item-
